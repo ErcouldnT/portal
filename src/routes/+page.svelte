483eadda2660d132883202dff15a2ed0d6bcf5ba
Ercode Techoglu <ercode@pm.me>
2024-06-05 06:52:34 +0300

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+
+	const authorizedExtensions = ['.jpg', '.jpeg', '.png', '.webp', '.exe'];
+</script>
+
+<form
+	class="p-10 flex flex-col gap-5 justify-center items-center"
+	method="post"
+	use:enhance
+	enctype="multipart/form-data"
+>
+	<div>
+		<!-- <label for="file">Upload your file</label> -->
+		<input
+			type="file"
+			id="file"
+			name="fileToUpload"
+			accept={authorizedExtensions.join(',')}
+			required
+		/>
+	</div>
+
+	<button class="btn variant-filled-primary" type="submit">Submit</button>
+</form>
